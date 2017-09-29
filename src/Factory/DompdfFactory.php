7688de4b5b68fb0ff4aed275baa44a08b56a354e
core23 <mail@core23.de>
2017-09-29 18:07:00 +0200

    Fixed CS

--- src/Factory/DompdfFactory.php
+++ src/Factory/DompdfFactory.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * (c) Christian Gripp <mail@core23.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Core23\DompdfBundle\Factory;
 
 use Dompdf\Dompdf;
