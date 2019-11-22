eda47693065aa43498f38e4a54524937d05138b6
core23 <mail@core23.de>
2019-11-22 18:28:28 +0100

    Remove old symfony <4.2 code

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -21,12 +21,8 @@ final class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder('core23_dompdf');
 
-        // Keep compatibility with symfony/config < 4.2
-        if (!method_exists(TreeBuilder::class, 'getRootNode')) {
-            $rootNode = $treeBuilder->root('core23_dompdf');
-        } else {
-            $rootNode = $treeBuilder->getRootNode();
-        }
+        $rootNode = $treeBuilder->getRootNode();
+
         \assert($rootNode instanceof ArrayNodeDefinition);
 
         $rootNode
