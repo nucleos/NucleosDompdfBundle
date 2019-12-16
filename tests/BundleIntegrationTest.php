10b0813cbffc70129916c7cc725b1d6717b226e7
core23 <mail@core23.de>
2019-12-16 22:08:44 +0100

    Add integration test

---
+++ tests/BundleIntegrationTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) Christian Gripp <mail@core23.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Core23\DompdfBundle\Tests;
+
+use Core23\DompdfBundle\Tests\App\AppKernel;
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Component\HttpKernel\Client;
+
+final class BundleIntegrationTest extends TestCase
+{
+    public function testStartup(): void
+    {
+        if (class_exists(KernelBrowser::class)) {
+            $client = new KernelBrowser(new AppKernel());
+        } else {
+            $client = new Client(new AppKernel());
+        }
+
+        $client->request('GET', '/test');
+
+        static::assertSame(200, $client->getResponse()->getStatusCode());
+    }
+}
