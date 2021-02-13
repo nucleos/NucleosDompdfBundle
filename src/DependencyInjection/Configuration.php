0b103089d211be94fd2167a5bae49844e459aed9
core23 <mail@core23.de>
2021-02-13 15:45:42 +0100

    Remove unnecessary assert

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -11,7 +11,6 @@ declare(strict_types=1);
 
 namespace Nucleos\DompdfBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -22,8 +21,6 @@ final class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder('nucleos_dompdf');
 
         $rootNode = $treeBuilder->getRootNode();
-
-        \assert($rootNode instanceof ArrayNodeDefinition);
 
         $rootNode
             ->children()
